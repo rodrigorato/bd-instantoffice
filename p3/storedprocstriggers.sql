2f9a5a50add2c73b9904f5ab6f2e944dd1400093
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2016-11-30 15:50:09 +0000

    Triggers working we thing

--- p3/storedprocstriggers.sql
+++ p3/storedprocstriggers.sql
@@ -5,6 +5,10 @@
 *************************/
 
 
+
+
+
+-- Trigger 1
 DROP TRIGGER IF EXISTS overlappedDates;
 DELIMITER //
 CREATE TRIGGER overlappedDates BEFORE INSERT ON oferta
@@ -34,3 +38,28 @@ DELIMITER ;
 
 -- Trigger testing statements
 -- Should fail after our populate.sql -- INSERT INTO oferta VALUES('2311N Los Robles Avenue', '127', '2009-07-29', '2013-01-03', 1233.99);
+-- Fim Trigger 1
+
+
+
+-- Trigger 2
+
+DROP TRIGGER IF EXISTS estadoPaga;
+DELIMITER //
+CREATE TRIGGER estadoPaga BEFORE INSERT ON paga
+    FOR EACH ROW
+    BEGIN
+        SET @estado_maxtimestamp = (SELECT MAX(time_stamp)
+                                    FROM estado
+                                    WHERE numero = new.numero);
+
+        IF @estado_maxtimestamp > new.data THEN
+            CALL ERR_ESTADOPAGA_TRIGGER; -- Calling non existing function because there are no exceptions.
+        END IF;
+            -- ELSE adicionar ao estado??
+    END //
+DELIMITER ;
+
+-- Trigger testing statements
+-- Should fail after teacher's populate.sql -- INSERT into paga values('2016-12', '2015-01-01 00:00:00', 'Paga');
+-- Fim Trigger 2
