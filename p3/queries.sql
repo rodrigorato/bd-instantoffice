1f1e6332bb4162b1e322a00c65e19c548f8ea974
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2016-12-01 17:22:31 +0000

    query 4 done

--- p3/queries.sql
+++ p3/queries.sql
@@ -3,8 +3,8 @@
 *				Grupo 19				*
 *************************/
 
--- Query #1
-SELECT e.morada, e.codigo -- espaços c/postos nunca alugados
+-- Query #1 -- espaços c/postos nunca alugados
+SELECT e.morada, e.codigo
 FROM espaco e, posto p
 WHERE e.morada = p.morada AND
       e.codigo = p.codigo_espaco AND
@@ -12,8 +12,8 @@ WHERE e.morada = p.morada AND
 GROUP BY e.morada, e.codigo;
 
 
--- Query #2
-SELECT morada -- edificios com #reservas > avg(#reservas)
+-- Query #2 -- edificios com #reservas > avg(#reservas)
+SELECT morada
 FROM aluga a
 GROUP BY morada
 HAVING count(*) >= (SELECT avg(r.num_reservas) as avg_num_reservas
@@ -21,8 +21,8 @@ HAVING count(*) >= (SELECT avg(r.num_reservas) as avg_num_reservas
                           FROM aluga a
                           GROUP BY morada) as r);
 
--- Query #3
-SELECT DISTINCT u.* -- users c/alugaveis fiscalizados por um so fiscal
+-- Query #3 -- users c/alugaveis fiscalizados por um so fiscal
+SELECT DISTINCT u.*
 FROM fiscaliza f, arrenda a, user u
 WHERE f.codigo = a.codigo
       AND a.morada = f.morada
@@ -30,36 +30,7 @@ WHERE f.codigo = a.codigo
 GROUP BY f.morada, f.codigo
 HAVING COUNT(DISTINCT f.id) = 1;
 
--- Query #4
-/*SELECT * -- montante pago p/espaco em 2016 (total ou p/postos)
-FROM oferta o, espaco e
-WHERE o.morada = e.morada
-      AND o.codigo = e.codigo;
-
-SELECT DISTINCT e.*, SUM(DATEDIFF(o.data_fim, o.data_inicio)*o.tarifa) as montante_pago_2016 -- montante por espaços pagos
-FROM paga p, aluga a, espaco e, oferta o, estado est
-WHERE p.numero = a.numero AND
-      a.morada = e.morada AND
-      a.codigo = e.codigo AND
-      a.codigo = o.codigo AND
-      a.morada = o.morada AND
-      est.numero = p.numero AND
-      est.estado = 'Aceite' AND
-      YEAR(est.time_stamp) = 2016
-GROUP BY a.morada, a.codigo
-UNION
-SELECT DISTINCT pos.morada, pos.codigo_espaco, SUM(DATEDIFF(o.data_fim, o.data_inicio)*o.tarifa) as montante_pago_2016 -- montante por postos pagos
-FROM paga p, aluga a, posto pos, oferta o, estado est
-WHERE p.numero = a.numero AND
-      a.morada = pos.morada AND
-      a.codigo = pos.codigo AND
-      a.codigo = o.codigo AND
-      a.morada = o.morada AND
-      est.numero = p.numero AND
-      est.estado = 'Aceite' AND
-      YEAR(est.time_stamp) = 2016
-GROUP BY pos.morada, pos.codigo_espaco;
-*/
+-- Query #4 -- montante pago p/espaco em 2016 (total ou p/postos)
 SELECT DISTINCT morada, codigo, SUM(DATEDIFF(data_fim, data_inicio)*tarifa) as montante_pago_2016 -- montante por espaços pagos
 FROM paga NATURAL JOIN aluga NATURAL JOIN espaco NATURAL JOIN oferta NATURAL JOIN  estado
 WHERE
