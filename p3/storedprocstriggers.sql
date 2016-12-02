58b82411a5cd671b569c3701972eb0dc6e2a1c4b
alice-d <alice.dourado@tecnico.ulisboa.pt>
2016-12-02 13:58:38 +0000

    mudanças nas variaveis, needs testing

--- p3/storedprocstriggers.sql
+++ p3/storedprocstriggers.sql
@@ -34,7 +34,15 @@ CREATE TRIGGER overlappedDates BEFORE INSERT ON oferta
 DELIMITER ;
 
 -- Trigger testing statements
--- Should fail after our populate.sql -- INSERT INTO oferta VALUES('2311N Los Robles Avenue', '127', '2009-07-29', '2013-01-03', 1233.99);
+/*
+these should work(with the populate.sql given by the teachers):
+insert into oferta values ('IST', 'Central', '2016-03-01', '2016-03-30', 19.99);
+insert into oferta values ('IST', 'Central', '2015-01-01', '2015-01-31', 19.99);
+these should fail(with the populate.sql given by the teachers):
+insert into oferta values ('IST', 'Central', '2016-01-05', '2016-01-10', 19.99);
+insert into oferta values ('IST', 'Central', '2015-12-01', '2016-01-20', 19.99);
+insert into oferta values ('IST', 'Central', '2016-01-20', '2016-12-30', 19.99);
+*/
 -- Fim Trigger 1
 
 
@@ -46,11 +54,12 @@ DELIMITER //
 CREATE TRIGGER estadoPaga BEFORE INSERT ON paga
     FOR EACH ROW
     BEGIN
-        SET @estado_maxtimestamp = (SELECT MAX(time_stamp)
-                                    FROM estado
-                                    WHERE numero = new.numero);
+    	DECLARE estado_maxtimestamp timestamp;
+        (SELECT MAX(time_stamp)
+         FROM estado
+         WHERE numero = new.numero);
 
-        IF @estado_maxtimestamp > new.data THEN
+        IF estado_maxtimestamp > new.data THEN
             CALL ERR_ESTADOPAGA_TRIGGER; -- Calling non existing function because there are no exceptions.
         END IF;
             -- ELSE adicionar ao estado??
@@ -63,19 +72,34 @@ DELIMITER //
 CREATE TRIGGER estadoPaga BEFORE INSERT ON estado
     FOR EACH ROW
     BEGIN
-    	--data paga >= data ultimo estado
-    	--! data paga < data ultimo estado
-    	set @paga_data = 	(select data
-    						from paga
-    						where numero = new.numero)
-    	if @paga_data < new.time_stamp THEN
-    		call ERR_ESTADOPAGA_TRIGGER
-    	end if;
+    	DECLARE paga_data timestamp;
+    	(SELECT data
+    	FROM paga
+    	WHERE numero = new.numero) into paga_data
+
+    	IF !(paga_data IS NULL) THEN
+       		IF paga_data < new.time_stamp THEN
+    			call ERR_ESTADOPAGA_TRIGGER
+    		END IF;
+    	end IF;
     END //
 DELIMITER ;
 
 
 -- Trigger testing statements
+/*
+should work:
+insert into estado values('2016-14','2016-01-01 00:00:00','Aceite');
+insert into estado values('2016-1','2014-01-01 00:00:00','Pendente');
+insert into paga values('2016-12','2016-01-02 00:00:00','Dinheiro');
+
+should fail:
+INSERT into paga values('2016-5', '2015-01-01 00:00:00', 'Paypal');
+Insert into estado values ('2016-1', '2017-01-01 00:00:00', 'Aceite');
+
+
+*/
+
 -- Should fail after teacher's populate.sql -- INSERT into paga values('2016-12', '2015-01-01 00:00:00', 'Paga');
 -- Insert into estado values ('2016-01', '2017-01-01 00:00:00', 'Aceite');
 -- Fim Trigger 2
