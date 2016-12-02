f40b913d9ea2d5c6621682449832d1b706267a56
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2016-12-02 16:03:47 +0000

    maybe?

---
+++ p3/web/setup.php
@@ -0,0 +1,12 @@
+<?php
+  	function getPDO(){ 
+  		$host = "db.ist.utl.pt";
+        $user ="ist181500";
+        $password = "tovv0904";
+        $dbname = $user;
+    
+        $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   		return $db;  
+  	}
+?>
