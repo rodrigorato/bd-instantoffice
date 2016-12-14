142fe46d01506449b1520c910488884425cd80d8
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2016-12-14 15:50:09 +0000

    totally cubed out

--- p4/olap_cube.sql
+++ p4/olap_cube.sql
@@ -1,3 +1,16 @@
-SELECT morada_codigo, data, AVG(montante_pago) as AVG_MONTANTE_PAGO
-FROM reserva_estrela
-GROUP BY CUBE(morada_codigo, data);
+SELECT edificio, espaco, posto, dia, mes, AVG(montante_pago)
+FROM reserva_estrela NATURAL JOIN data_dim NATURAL JOIN local_dim
+GROUP BY morada_codigo, data
+UNION
+SELECT edificio, espaco, posto, null, null, AVG(montante_pago) as AVG_MONTANTE_PAGO
+FROM reserva_estrela NATURAL JOIN data_dim NATURAL JOIN local_dim
+GROUP BY (morada_codigo)
+UNION
+SELECT null,null, null, dia, mes, AVG(montante_pago) as AVG_MONTANTE_PAGO
+FROM reserva_estrela NATURAL JOIN data_dim NATURAL JOIN local_dim
+GROUP BY (data)
+UNION
+SELECT null, null, null, null, null, AVG(montante_pago) as AVG_MONTANTE_PAGO
+FROM reserva_estrela NATURAL JOIN data_dim NATURAL JOIN local_dim;
+
+
